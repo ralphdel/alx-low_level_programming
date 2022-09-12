4a4aeaf885e6685e9e07fed79886fe63287c24ab
ralphdel <ralphdel14@yahoo.com>
2022-09-12 08:38:32 -0700

    using the put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*
+ *print hello world to standard output
+ *Using only the put function
+ */
+
+#include<stdio.h>
+
+int main()
+{
+  puts("Programming is like building a multilingual puzzle");
+  return (0);
+}
