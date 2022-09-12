b32e59fce9deffc6cb459284dcfd618b03306374
ralphdel <ralphdel14@yahoo.com>
2022-09-12 09:20:37 -0700

    initial code correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 /*
  *print hello world to standard output
  *Using only the put function
+ * main:print the string in the put function
  */
 
 #include<stdio.h>
 
-int main()
+int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
