ac347d1d8dcd9b117991bfc3e4fcaefa050dd2a8
ralphdel <ralphdel14@yahoo.com>
2022-09-12 10:35:00 -0700

    print function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Use the printf function
+ *
+ * Description: 'Using the main function'
+ *this program prints  "with proper grammar, but the outcome is a piece of art"
+ *Return : 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
