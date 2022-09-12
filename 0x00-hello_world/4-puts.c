c6ada5e13815bb858944dff9bdcc69cfa3bf7fa2
ralphdel <ralphdel14@yahoo.com>
2022-09-12 09:41:03 -0700

    resolved betty documentation issue

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-/*
- *print hello world to standard output
- *Using only the put function
- * main:print the string in the put function
- */
-
 #include<stdio.h>
+/**
+ * main-print the string in the put function
+ *
+ * Description: 'Using the main function'
+ *
+ *Return: Always 0
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
