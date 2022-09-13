344a86d68df43a1b24c8486edb567ce0d9b596bc
ralphdel <ralphdel14@yahoo.com>
2022-09-12 21:55:46 -0700

    print corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main - Using the main function
+ * main - Entry point
  *
  * Description: 'Using the main function'
- *this program prints  "with proper grammar, but the outcome is a piece of art"
- *Return : 0
+ *
+ * Return: Always  0
  */
 
 int main(void)
