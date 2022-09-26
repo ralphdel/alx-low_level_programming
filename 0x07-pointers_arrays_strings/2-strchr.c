1fd642c3c40c6c1b3c54a27364f0d68f7a622a9a
ralphdel <ralphdel14@yahoo.com>
2022-09-26 16:14:58 -0700

    string location updated

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,11 @@ char *_strchr(char *s, char c)
 
 	for (i = 0 ; *(s + i); i++)
 	{
-		if ((*(s + i)  == c)
+		if ((*(s + i)  == c))
 				return (s + i);
 	}
 	if (*(s + i) == c)
 		return (s + i);
-
 	return (0);
 }
 
